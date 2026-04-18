32f902c5277be555ba6224c449613b7e3dd7ce3d
Namida Verasche <namida.verasche@gmail.com>
2026-04-18 22:32:15 +1200

    install.sh checks files individually.

--- install.sh
+++ install.sh
@@ -4,8 +4,14 @@ source /opt/config/mod/.shell/0.sh
 
 if [ ! -f "${KLIPPER_DIR}/klippy/extras/ifs_jacker.py" ]; then
     ln -s ${MOD_CONF}/mod_data/plugins/ifs_jacker/ifs_jacker.py ${KLIPPER_DIR}/klippy/extras/ifs_jacker.py
+fi
+if [ ! -f "${KLIPPER_DIR}/klippy/extras/ifs_jacker_temperature_sensor.py" ]; then
     ln -s ${MOD_CONF}/mod_data/plugins/ifs_jacker/ifs_jacker_temperature_sensor.py ${KLIPPER_DIR}/klippy/extras/ifs_jacker_temperature_sensor.py
+fi
+if [ ! -f "${KLIPPER_DIR}/klippy/extras/ifs_jacker_fan.py" ]; then
     ln -s ${MOD_CONF}/mod_data/plugins/ifs_jacker/ifs_jacker_fan.py ${KLIPPER_DIR}/klippy/extras/ifs_jacker_fan.py
+fi
+if [ ! -f "${KLIPPER_DIR}/klippy/extras/ifs_jacker_led.py" ]; then
     ln -s ${MOD_CONF}/mod_data/plugins/ifs_jacker/ifs_jacker_led.py ${KLIPPER_DIR}/klippy/extras/ifs_jacker_led.py
 fi
 
